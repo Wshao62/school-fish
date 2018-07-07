5740314b5d6b705c73f4f4cb77182863629e1f30
sklonely <asd195375@gmail.com>
2018-07-07 14:00:50 +0800

    modbus更新

--- modbus.py
+++ modbus.py
@@ -1,29 +1,36 @@
-# import自動修復 程式碼片段Stste
-lestModName = ""
-while 1:
+import sys
+import logging
+import modbus_tk
+import modbus_tk.defines as cst
+import modbus_tk.modbus_tcp as modbus_tcp
+logger = modbus_tk.utils.create_logger("console")
+if __name__ == "__main__":
     try:
-        import sys
-        import os
-        sys.path.append(sys.path[0] + '/mods/')  # 將自己mods的路徑加入倒python lib裡面
-        # 要import的東西放這下面
-        from pymodbus.client.sync import ModbusTcpClient
-    except (ModuleNotFoundError, ImportError):  # python import error
-        err = str(sys.exc_info()[1])[17:-1]
-        if (lestModName != err):
-            print("缺少mod: " + err + " 正在嘗試進行安裝")
-            os.system("pip install " + err)
-            lestModName = err
-        else:
-            print("無法修復import問題 請人工檢查", "mod name: " + err)
-            sys.exit()
-    else:
-        del lestModName
-        break
-# import自動修復 程式碼片段
-
-
-client = ModbusTcpClient('127.0.0.1')
-client.write_coil(1, True)
-result = client.read_coils(1,1)
-print(result.bits[0])
-client.close()
+        # 连接MODBUS TCP从机
+        master = modbus_tcp.TcpMaster(host="192.168.1.15")
+        master.set_timeout(5.0)
+        logger.info("connected")
+        # 读保持寄存器
+        logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 16))
+        # 读输入寄存器
+        logger.info(master.execute(1, cst.READ_INPUT_REGISTERS, 0, 16))
+        # 读线圈寄存器
+        logger.info(master.execute(1, cst.READ_COILS, 0, 16))
+        # 读离散输入寄存器
+        logger.info(master.execute(1, cst.READ_DISCRETE_INPUTS, 0, 16))
+        # 单个读写寄存器操作
+        # 写寄存器地址为0的保持寄存器
+        logger.info(master.execute(1, cst.WRITE_SINGLE_REGISTER, 0, output_value=21))
+        logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 1))
+        # 写寄存器地址为0的线圈寄存器，写入内容为0（位操作）
+        logger.info(master.execute(1, cst.WRITE_SINGLE_COIL, 0, output_value=0))
+        logger.info(master.execute(1, cst.READ_COILS, 0, 1))
+        # 多个寄存器读写操作
+        # 写寄存器起始地址为0的保持寄存器，操作寄存器个数为4
+        logger.info(master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 0, output_value=[20,21,22,23]))
+        logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 4))
+        # 写寄存器起始地址为0的线圈寄存器
+        logger.info(master.execute(1, cst.WRITE_MULTIPLE_COILS, 0, output_value=[0,0,0,0]))
+        logger.info(master.execute(1, cst.READ_COILS, 0, 4))
+    except modbus_tk.modbus.ModbusError as e:
+        logger.error("%s- Code=%d" % (e, e.get_exception_code()))
